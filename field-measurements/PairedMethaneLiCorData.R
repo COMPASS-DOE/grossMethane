28bfe7acbec8a89c5057ef69f45f59d629605ac8
mitchellsmith21 <mitchellsmith21@users.noreply.github.com>
2022-07-12 16:20:47 -0400

    Update PairedMethaneLiCorData.R

--- field-measurements/PairedMethaneLiCorData.R
+++ field-measurements/PairedMethaneLiCorData.R
@@ -11,6 +11,7 @@
 #load ggplot2
 library(ggplot2)
 library(dplyr)
+library(lubridate)
 
 #read in collar ids and data
 collars <- read.csv("cores_collars.csv")
@@ -49,9 +50,82 @@ data$Origin <- recode_factor(data$Origin,
                 "MSLE" = "midstream",
                 "LSLE" = "upstream")
 
+#create timestamp
+data$timestamp <- paste(data$date, data$time)
+data$timestamp <- mdy_hms(data$timestamp, tz="EST")
+
 #first summary plot of CO2 fluxes
-ggplot(data = data, aes(Experiment, FCO2_dry)) +
+ggplot(data = data, aes(Experiment, FCO2_dry,)) +
   geom_boxplot()
 
+# FCO2_dry, TS_mean
+ggplot(data = data, aes(FCO2_dry, TS_mean)) +
+         geom_point(stat = "identity")
+
+# TS_mean, FCO2_dry -> without outliers
+ggplot(data = data[data$FCO2_dry<25,], aes(TS_mean, FCO2_dry)) +
+  geom_point()
+
+# date, FCO2_dry -> advanced
+ggplot(data = data, aes(date,FCO2_dry)) +
+  geom_boxplot(aes(group = date, fill = Origin))
 
 
+
+#Date series plots -> CO2
+ggplot(data = data, aes(date, FCO2_dry)) +
+  geom_point()
+
+#Date series plots -> CH4
+ggplot(data = data, aes(date, FCH4_dry)) +
+  geom_point()
+
+#Date series plots -> TS_mean
+ggplot(data = data, aes(date, TS_mean)) +
+  geom_point()
+
+#Date series plots -> SWC_mean
+ggplot(data = data, aes(date, SWC_mean)) +
+  geom_point()
+
+#What is the effect of temp on CO2
+# As Temp increases, CO2 also increases
+ggplot(data = data[data$FCO2_dry<25,], aes(TS_mean, FCO2_dry)) +
+  geom_point()
+
+#What is the effect of temp on CH4 
+# As Temp increases, CH4 remains the same
+ggplot(data = data[data$FCH4_dry<200,], aes(TS_mean, FCH4_dry)) +
+  geom_point()
+
+#What is the effect of SWC on CO2
+# As SWC increases, CO2 remains the same
+ggplot(data = data[data$FCO2_dry<25,], aes(SWC_mean,FCO2_dry)) +
+  geom_point()
+
+#What is the effect of SWC on CH4
+# As SWC increases, CH4 increases slightly  *filter out negative values for CH4
+ggplot(data = data[data$FCH4_dry<200,], aes(SWC_mean,FCH4_dry)) +
+  geom_point()
+
+#Filter g_low and Control
+lowcontrol <- filter(data, Origin == "g_low" & 
+                            Experiment == "Control")
+
+
+#Get boxes to be colored by origin
+# helpful hint - use [] to filter out outliers
+
+#200 for CH4
+ggplot(data = data[data$FCH4_dry<200,], 
+       aes(Experiment, FCH4_dry,)) +
+  geom_boxplot()
+
+# Good graph
+ggplot(data = data, aes(date,TS_mean)) +
+  geom_boxplot(aes(group = date, fill = Origin))
+
+# timestamp test
+ggplot(data, aes(hour(timestamp), FCO2_dry, color = TS_mean)) +
+  geom_point() +
+  facet_wrap(~Origin, scales = "free")
