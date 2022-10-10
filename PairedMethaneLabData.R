eee117922886580b3b46dff16c7bfe04c321f700
Kendalynn Morris <kendalynn.morris@pnnl.gov>
2022-10-10 15:58:40 -0400

    clean-up paired data integration
    rewrite initial analysis
    trim fat

--- PairedMethaneLabData.R
+++ PairedMethaneLabData.R
@@ -1,7 +1,7 @@
 # This code is for analyzing
 # gross methane fluxes from GCREW soils.
 # 13C tracer incubations were conducted the last week of July 22
-# in wetland edge (aka lowland aka transition) and upland soils
+# soils from wetland edge (aka lowland aka transition) and upland collars
 
 # September 2022
 # Kendalynn A. Morris
@@ -10,11 +10,13 @@ library(dplyr)
 library(ggplot2)
 library(ggpmisc)
 library(car)
+library(rcompanion)
 
-Olabs <- c("lowland", "midslope", "upslope", "midstream", "upstream")
-Llabs <- c("lowland", "midslope", "upslope")
+Olabs <- c("lowland", "midslope", "upland", "midstream", "upstream")
+Llabs <- c("lowland", "midslope", "upland")
 
-#read incubation data
+#Read Picarro data
+
 # Get names of data files
 files <- list.files("picarro/", pattern = "*.csv", full.names = TRUE)
 # Helper function
@@ -23,7 +25,7 @@ read_file <- function(f) {
     read_csv(f, col_types = "ccdcddcddddddddddddddddddddddccccc") %>%
         mutate(File = basename(f))
 }
-# Read in and pre-process data
+# select desired columns, correct for dilution with zero air
 lapply(files, read_file) %>%
     bind_rows() %>%
     # filter out standards and clean up columns
@@ -35,97 +37,130 @@ lapply(files, read_file) %>%
            `HR 12CH4 Mean`, `HR 12CH4 Std`,
            `HR 13CH4 Mean`, `HR 13CH4 Std`,
            `HR Delta iCH4 Mean`, `HR Delta iCH4 Std`,
+           #ignoring dilution of CO2 with zero air for now
            `12CO2 Mean`, `12CO2 Std`,
            `13CO2 Mean`, `13CO2 Std`,
-           `Delta 13CO2 Mean`, `Delta 13CO2 Std`,
-           notes) %>%
-    # calculate elapsed time for each sample
-    arrange(id, round) %>%
-    group_by(id) %>%
-    mutate(time_days = difftime(Timestamp, min(Timestamp),
-                                units = "days"),
-           time_days = as.numeric(time_days)) -> deltas
-
-#write.csv(deltas, "incDeltas.csv")
+           `Delta 13CO2 Mean`, `Delta 13CO2 Std`) -> picDat
 
 #read in soil moisture data
-soil <- read.csv("field-measurements/July22_soilmoisture.csv")
-g_dat <- merge(soil, deltas, by = "id")
+smDat <- read_csv("field-measurements/July22_soilmoisture.csv",
+                  col_types = "ccdddcc")
+
+#combine lab data (no flux data yet)
+labDat <- merge(smDat, picDat, by = "id")
 #calculate soil dry mass and fresh water mass
-g_dat$mass <- g_dat$jdry - g_dat$jempty
-g_dat$sm <- (g_dat$jfresh - g_dat$jdry)/g_dat$mass
+labDat$mass <- labDat$jdry - labDat$jempty
+labDat$sm <- (labDat$jfresh - labDat$jdry)/labDat$mass
 
+#select columns from not-a-toy output
+#name of data frame might change!
 incdat_out %>%
-    mutate(Collar = as.character(id)) %>%
-    select(-vol, -id) -> incdat_join
+    select(-vol) -> incdat_join
 
-g_dat %>%
+#rename columns,
+#correct for 50% dilution of injected sample volume
+#should std's be adjusted???
+labDat %>%
     mutate(Mppm = `HR 12CH4 Mean` * 2,
-           Mstd = `HR 12CH4 Std` * 2,
+           Mstd = `HR 12CH4 Std`,
            M13ppm = `HR 13CH4 Mean` * 2,
-           M13std = `HR 13CH4 Std` * 2,
+           M13std = `HR 13CH4 Std`,
            Cppm = `12CO2 Mean` * 2,
-           Cstd = `12CO2 Std` * 2,
+           Cstd = `12CO2 Std`,
            C13ppm = `13CO2 Mean` * 2,
-           C13std = `13CO2 Mean` * 2,
-           Collar = as.character(id)) %>%
-    select(-id, -jempty, -jfresh, -jdry, -vol,
+           C13std = `13CO2 Std`,
+           delC = `Delta 13CO2 Mean`,
+           delCstd = `Delta 13CO2 Std`,
+           delM = `HR Delta iCH4 Mean`,
+           delMset = `HR Delta iCH4 Std`) %>%
+    select(-jempty, -jfresh, -jdry, -vol,
            -`HR 12CH4 Mean`, -`HR 12CH4 Std`,
            -`HR 13CH4 Mean`, -`HR 13CH4 Std`,
            -`12CO2 Mean`, -`12CO2 Std`,
            -`13CO2 Mean`, -`13CO2 Std`,
-           -notes, -Timestamp, -time_days) %>%
-        left_join(incdat_join, by = c("Collar", "round")) %>%
+           -`Delta 13CO2 Mean`, -`Delta 13CO2 Std`,
+           -`HR Delta iCH4 Mean`, -`HR Delta iCH4 Std`,
+           -Timestamp) %>%
+        left_join(incdat_join, by = c("id", "round")) %>%
     select(order(colnames(.))) %>%
-    select(Collar, round, order, Location, Origin,
-           mass, sm, everything()) -> data
+    select(id, round, order, Location, Origin,
+           mass, sm, Pt, Ct, everything()) -> data
 
-#calculate umol of gas per g dry soil per day for Production
-data$umolPg <- (data$Pt * 0.0446)/g_dat$mass
-#calculate umol of gas per g dry soil per day for Consumption
+#calculate umol of CH4 per g dry soil per day for Production
+data$umolPg <- (data$Pt * 0.0446)/data$mass
+#calculate umol of CH4 per g dry soil per day for Consumption
 data$umolCg <- (data$Ct * 0.0446)/data$mass
 
-#are data normally distributed?
-par(mfrow = c(2, 3))
-for (x in 10:14) {
-    var = names(g_dat)[x]
-    i <- g_dat[,x]
-    hist(i,
-         main = paste(var))
-}
-#fluxes should be transformed
-dev.off()
-
-hist(log10(g_dat$umolPg + 50))
-g_dat$logP <- log10(g_dat$umolPg + 50)
-hist(sqrt(g_dat$umolKg + 83))
-g_dat$rtK <- sqrt(g_dat$umolPg + 83)
-
 #read in field flux data for July 27th and 29th
-f_dat <- read.csv("field-measurements/licorRTA.csv")
+#the two days when soil was collected for incubations
+fDat <- read_csv("field-measurements/licorRTA.csv",
+                 col_types = "cdcccccddddd")
+fDat %>%
+    filter(date %in% c("7/27/2022", "7/29/2022")) %>%
+    mutate(id = Collar) %>%
+    select(-Collar, -date, -Origin, -Location) -> incDays
 
-f_dat %>%
-  filter(date %in% c("2022-07-27", "2022-07-29")) %>%
-  select(-X) -> f_dat4g_dat
-
-g_dat %>%
-  rename(Collar = id) %>%
-  relocate(c(jempty, jfresh, jdry), .after = rtK) %>%
-  left_join(f_dat4g_dat, by = "Collar") -> data
+#data Ready-To-Analyze
+data %>%
+    left_join(incDays, by = "id") -> dataRTA
 
 #write.csv(data, "PairedData.csv")
 
-consumption <- aov(rtK ~ Location + sm,
-               data = data)
-Anova(consumption, type="III")
-par(mfrow=c(2,2))
-plot(consumption)
-dev.off()
+#prelimnary analysis
 
-production <- aov(logP ~ Location + sm,
-                   data = data)
+#are data normally distributed?
+par(mfrow = c(4,4))
+for (x in 6:32) {
+    var = names(dataRTA)[x]
+    i <- data[,x]
+    hist(i,
+         main = paste(var))
+}
+par(mfrow = c(1,1))
+#fluxes should be transformed
+
+#fragile code here!
+
+#first round, so not influence by time
+#nor duplicate values
+CPdat <- data[data$round == "T1",]
+
+###
+#production
+hist(CPdat$umolPg)
+qqnorm(CPdat$umolPg);qqline(CPdat$umolPg)
+transformTukey(CPdat$umolPg,
+               start = -10, end = 10, int = 0.01,
+               plotit = TRUE, verbose = FALSE, statistic = 1)
+#lamba = 0.26
+hist((CPdat$umolPg)^0.26)
+qqnorm(CPdat$umolPg^0.26);qqline(data$CPdat^0.26)
+
+#analysis
+production <- aov(umolPg^0.26 ~ Origin + Location + sm,
+                          data = CPdat)
 Anova(production, type="III")
 par(mfrow=c(2,2))
 plot(production)
+dev.off()
+
+###
+#consumption
+###
+hist(CPdat$umolCg)
+qqnorm(CPdat$umolCg);qqline(CPdat$umolCg)
+transformTukey(CPdat$umolCg,
+               start = -10, end = 10, int = 0.01,
+               plotit = TRUE, verbose = FALSE, statistic = 1)
+#lamba = -0.58
+hist(-1 * (CPdat$umolCg)^-0.58)
+qqnorm(-1 * (CPdat$umolCg)^-0.58);qqline(-1 * (CPdat$umolCg)^-0.58)
+
+#analysis
+consumption <- aov((-1 * (umolCg)^-0.58) ~ Origin + Location + sm,
+               data = CPdat)
+Anova(consumption, type="III")
+par(mfrow=c(2,2))
+plot(consumption)
 dev.off()
 
